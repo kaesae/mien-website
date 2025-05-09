2ee1c7bc37670c76a6349d86dc61fac900861e79
kaesae <kae.saetern@live.com>
2025-05-09 14:50:27 -0700

    Footer added Resume download

--- personal-projects/mien-website/client/src/Components/Footer.js
+++ personal-projects/mien-website/client/src/Components/Footer.js
@@ -1,9 +1,9 @@
 
-
 const Footer = () => {
     return (
         <footer id='footer'>
             <ul>
+                <li><a href='/swe.docx' download="Kae_Saetern_Software_Engineer_Resume.docx">Resume</a></li>
                 <li><a href="https://kaesae.github.io/Portfolio/">Portfolio</a></li>
                 <li><a href="https://github.com/kaesae">Github</a></li>
                 <li><a href="https://www.linkedin.com/in/kae-saetern">LinkedIn</a></li>
